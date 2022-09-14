74558639fc2bc816d1b99d7dce7e51ef0f62eb0a
debora <debora.r.serra@gmail.com>
2022-09-14 16:05:17 -0300

    fix: fix req19

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -3,7 +3,7 @@ import { zVehicleSchema } from './IVehicle';
 
 export const zMotorcycleSchema = zVehicleSchema.merge(z.object({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().gte(2500),
+  engineCapacity: z.number().lte(2500).positive(),
 }));
 
 export type IMotorcycle = z.infer<typeof zMotorcycleSchema>;
